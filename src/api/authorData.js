99f24a54d1535d1982998591bba7391f8633800c
zeudy <143110888@upmh.edu.mx>
2017-08-10 17:36:14 -0500

    first commit

---
+++ src/api/authorData.js
@@ -0,0 +1,23 @@
+module.exports = {
+    authors:
+    [
+        {
+            id: 'zeudy-flores',
+            firstName:'Zeudy',
+            lastName:'Flores'
+
+        },
+        {
+            id: 'scott-allen',
+            firstName:'Scoot',
+            lastName:'Allen'
+
+        },
+        {
+            id: 'dan-perro',
+            firstName:'Dan',
+            lastName:'Perro'
+
+        }
+    ]
+};
---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var React = require('react');
+var About = React.createClass({
+    render: function (){
+        return (
+            <div>
+                <h1> Abooout </h1>
+                <p>
+                    This applications uses...:
+                    <ul>
+                        <li>React</li>
+                        <li>React Router</li>
+                        <li>Flux</li>
+                        <li>Node</li>
+                        <li>Gulp</li>
+                        <li>Browserify</li>
+                        <li>Bootstrap</li>
+
+                        </ul>
+
+                        </p>
+                        </div>
+        );
+    }
+});
+
+
+module.exports = About;
